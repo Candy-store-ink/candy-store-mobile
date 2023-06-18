f377a2f98c9ecce6e84688bd8da0e8ce65208f43
AndriyKorzh <andriikorhz@gmail.com>
2023-06-18 23:05:57 +0300

    add logic for splash screen

---
+++ app/src/main/java/com/example/candystore/ui/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.candystore.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.candystore.R
+
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel : MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+        }
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
