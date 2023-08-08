b228160932f8313af4af1550fc9167437e8c36aa
AndriiKorhz <andriikorhz@gmail,com>
2023-08-08 21:13:30 +0300

    create retrofit instance for user auth

---
+++ app/src/main/java/com/example/candystore/api/AuthRetrofitInstance.kt
@@ -0,0 +1,30 @@
+package com.example.candystore.api
+
+import com.example.candystore.utils.Constants.Companion.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class AuthRetrofitInstance {
+    companion object {
+        private val retrofit by lazy {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+
+        val authApi by lazy {
+            retrofit.create(AuthApi::class.java)
+        }
+    }
+}
