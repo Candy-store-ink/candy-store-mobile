f17340d4bd265bde2ee797b5cd740a8ddcd2a7e1
AndriiKorhz <andriikorhz@gmail,com>
2023-08-08 21:51:47 +0300

    write api method for check user token

--- app/src/main/java/com/example/candystore/api/AuthApi.kt
+++ app/src/main/java/com/example/candystore/api/AuthApi.kt
@@ -2,13 +2,21 @@ package com.example.candystore.api
 
 import com.example.candystore.models.UserAuth
 import com.example.candystore.models.UserAuthResponse
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface AuthApi {
+
+    @GET("auth/token")
+    suspend fun checkUser(
+        @Query("token" ) token: String
+    ) : Response<UserAuthResponse>
 
     @POST("auth/login")
     suspend fun getUser(
         @Body userAuth: UserAuth
-    ): UserAuthResponse
+    ): Response<UserAuthResponse>
 }
