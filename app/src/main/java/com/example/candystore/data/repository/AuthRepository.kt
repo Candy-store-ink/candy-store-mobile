adea734571fffa10fa2f8aa28842bce36418c3e8
AndriiKorhz <andriikorhz@gmail,com>
2023-08-11 19:56:09 +0300

    write base class for repositories

--- app/src/main/java/com/example/candystore/data/repository/AuthRepository.kt
+++ app/src/main/java/com/example/candystore/data/repository/AuthRepository.kt
@@ -3,11 +3,12 @@ package com.example.candystore.data.repository
 import com.example.candystore.data.api.AuthRetrofitInstance
 import com.example.candystore.data.models.UserAuth
 
-class AuthRepository {
+class AuthRepository : BaseRepository(){
 
     suspend fun validateUser(token: String) =
         AuthRetrofitInstance.authApi.checkUser(token)
 
-    suspend fun getUser(userAuth: UserAuth) =
+    suspend fun login(userAuth: UserAuth) = safeApiCall {
         AuthRetrofitInstance.authApi.login(userAuth)
+    }
 }
