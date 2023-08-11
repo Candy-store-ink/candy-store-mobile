443b05c5b7d77e2069cfa7438f60428778212ca9
AndriiKorhz <andriikorhz@gmail,com>
2023-08-11 23:42:21 +0300

    change base url

--- app/src/main/java/com/example/candystore/utils/Constants.kt
+++ app/src/main/java/com/example/candystore/utils/Constants.kt
@@ -2,6 +2,6 @@ package com.example.candystore.utils
 
 class Constants {
     companion object {
-        const val BASE_URL = "https://api/v1"
+        const val BASE_URL = "https://candy-store-app-99a6461d7f45.herokuapp.com/api/v1/"
     }
 }
