052c1c37fa2b10bdd19c36afa6bcba327c0277e2
AndriiKorhz <andriikorhz@gmail,com>
2023-08-11 23:41:15 +0300

    add nav graph realisation

--- app/src/main/java/com/example/candystore/ui/MainActivity.kt
+++ app/src/main/java/com/example/candystore/ui/MainActivity.kt
@@ -4,36 +4,39 @@ import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.datastore.preferences.SharedPreferencesMigration
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
 import com.example.candystore.R
 import com.example.candystore.data.repository.AuthRepository
-import com.example.candystore.ui.viewmodels.MainViewModel
-import com.example.candystore.ui.viewmodels.MainViewModelProviderFactory
+import com.example.candystore.databinding.ActivityMainBinding
+import com.example.candystore.ui.viewmodels.AuthViewModel
+import com.example.candystore.ui.viewmodels.ViewModelProviderFactory
 
 val Context.dataStore by preferencesDataStore("data_store")
 class MainActivity : AppCompatActivity() {
 
-    lateinit var viewModel: MainViewModel
-    lateinit var
+    lateinit var viewModel: AuthViewModel
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
 
         val authRepository = AuthRepository()
-        val mainViewModelProviderFactory = MainViewModelProviderFactory(authRepository)
+        val viewModelProviderFactory = ViewModelProviderFactory(authRepository)
         viewModel = ViewModelProvider(
             this,
-            mainViewModelProviderFactory
-        )[MainViewModel::class.java]
+            viewModelProviderFactory
+        )[AuthViewModel::class.java]
 
         installSplashScreen().apply {
             setKeepOnScreenCondition {
                 viewModel.isLoading.value
             }
         }
-        setContentView(R.layout.activity_login)
+        setContentView(view)
 
     }
 }
