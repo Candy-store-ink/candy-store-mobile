30ba83bae9d0b9f52fe3b096d0f00eb2396517fe
AndriiKorhz <andriikorhz@gmail,com>
2023-08-11 21:46:40 +0300

    add VM creating in BaseFragment.kt

--- app/src/main/java/com/example/candystore/ui/base/BaseFragment.kt
+++ app/src/main/java/com/example/candystore/ui/base/BaseFragment.kt
@@ -6,11 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewbinding.ViewBinding
 import com.example.candystore.data.repository.BaseRepository
+import com.example.candystore.ui.viewmodels.ViewModelProviderFactory
 
 abstract class BaseFragment<VM: ViewModel, B: ViewBinding, R: BaseRepository> : Fragment() {
     protected lateinit var binding: B
+    protected lateinit var viewModel: VM
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +21,9 @@ abstract class BaseFragment<VM: ViewModel, B: ViewBinding, R: BaseRepository> : 
         savedInstanceState: Bundle?
     ): View? {
         binding = getViewBinding(inflater, container)
+
+        val factory = ViewModelProviderFactory(getFragmentRepository())
+        viewModel = ViewModelProvider(this, factory)[getViewModel()]
         return binding.root
     }
 
