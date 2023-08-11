95092602e666d5ec00d23f4f99d1dbd4ec543155
AndriiKorhz <andriikorhz@gmail,com>
2023-08-11 21:41:18 +0300

    refactor VM factory

---
+++ app/src/main/java/com/example/candystore/ui/viewmodels/ViewModelProviderFactory.kt
@@ -0,0 +1,22 @@
+package com.example.candystore.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.candystore.data.repository.AuthRepository
+import com.example.candystore.data.repository.BaseRepository
+import java.lang.IllegalArgumentException
+
+@Suppress("UNCHECKED_CAST")
+class ViewModelProviderFactory(
+    private val repository: BaseRepository
+) : ViewModelProvider.Factory {
+    private val VM_NOT_FOUND = "ViewModelClass not found"
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+       return when {
+           modelClass.isAssignableFrom(
+               AuthViewModel::class.java
+           ) -> AuthViewModel(repository as AuthRepository) as T
+           else -> throw IllegalArgumentException(VM_NOT_FOUND)
+       }
+    }
+}
