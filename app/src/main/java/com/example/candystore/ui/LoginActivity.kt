71aff48761ff9b476d1f4d8f6600ef17cd5fe370
AndriiKorhz <andriikorhz@gmail,com>
2023-08-12 20:37:29 +0300

    rename activity class & xml

--- app/src/main/java/com/example/candystore/ui/LoginActivity.kt
+++ app/src/main/java/com/example/candystore/ui/LoginActivity.kt
@@ -1,33 +1,27 @@
 package com.example.candystore.ui
 
-import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.NavHostFragment
-import com.example.candystore.R
 import com.example.candystore.data.UserPreferences
 import com.example.candystore.data.repository.AuthRepository
-import com.example.candystore.databinding.ActivityMainBinding
+import com.example.candystore.databinding.ActivityLoginBinding
 import com.example.candystore.ui.viewmodels.AuthViewModel
 import com.example.candystore.ui.viewmodels.ViewModelProviderFactory
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.observeOn
 import kotlinx.coroutines.launch
 
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     lateinit var viewModel: AuthViewModel
-    lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
 
         val authRepository = AuthRepository()
@@ -49,7 +43,7 @@ class MainActivity : AppCompatActivity() {
 
         lifecycleScope.launch {
             userPreferences.authToken.collect {token ->
-                Toast.makeText(this@MainActivity, token ?: "Token is null", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@LoginActivity, token ?: "Token is null", Toast.LENGTH_SHORT).show()
 
             }
         }
